46d3d474b725a49771318370ca24482b93a49132
Andrew Waller <andrewhwaller@gmail.com>
2019-04-18 23:35:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
 
     current_hour = Time.now.hour
 
-    if currrent_hour =< 1200
+    if currrent_hour <= 1200
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
